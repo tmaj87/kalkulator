82613a9b31edeef01165f359be96e331b63d7e98
Tomasz Maj <tmaj.pl@gmail.com>
2016-10-26 00:23:54 +0200

    finally

--- draw/BaseButtons.php
+++ draw/BaseButtons.php
@@ -1,22 +1,21 @@
-
-    <div class="buttons col-md-4 col-md-offset-1">
-        <div class="btn-group btn-group-justified" role="group">
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-primary btn-lg"
-                        name="base">NaOH
-                </button>
-            </div>
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-lg"
-                        name="base">KOH
-                </button>
-            </div>
+<div class="buttons col-md-4 col-md-offset-1">
+    <div class="btn-group btn-group-justified" role="group">
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-primary btn-lg"
+                    name="base">NaOH
+            </button>
+        </div>
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-lg"
+                    name="base">KOH
+            </button>
         </div>
     </div>
+</div>
 
-    <div class="col-md-6">
-        <div class="input-group input-group-lg">
-            <span class="input-group-addon" id="sizing-addon1">Niezmydlonych olejów</span>
-            <input type="number" class="form-control" placeholder="%" min="0" max="10">
-        </div>
+<div class="col-md-7">
+    <div class="input-group input-group-lg">
+        <span class="input-group-addon" id="sizing-addon1">Niezmydlonych olejów</span>
+        <input type="number" class="form-control" placeholder="%" min="0" max="10">
     </div>
+</div>
