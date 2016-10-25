2d49c1cbf18ca0ac7b3925c0bcabb58d36e2153c
Tomasz Maj <tmaj.pl@gmail.com>
2016-10-25 20:57:12 +0200

    further down the road

--- draw/TabContent.php
+++ draw/TabContent.php
@@ -5,14 +5,13 @@
 
             <?php foreach (Parser::getDatabase() as $record) {
                 $id = Parser::getIdFor($record['nazwa']);
-                $isActive = !empty($form_data[$id]['gram']);
                 ?>
 
                 <tr class="form_block <?php echo $record['typ']; ?>
                     <?php echo $isActive ? '' : ' style="display: none;"'; ?>">
 
                     <td>
-                        <input class="form-control <?php echo $isActive ? ' focus' : ''; ?>"
+                        <input class="form-control"
                                type="number"
                                style="width: 90px"
                                name="<?php echo $id; ?>" min="1"
