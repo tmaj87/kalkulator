db21f70c3de4d1bc2d16658f70c8a94fefadadb5
Tomasz Maj <tmaj.pl@gmail.com>
2016-10-25 19:55:16 +0200

    #refactoring

--- draw/Form.php
+++ draw/Form.php
@@ -1,32 +1,5 @@
 <form id="form" action="#" method="post" class="form-inline text-center">
-
     <?php Draw::BaseButtons(); ?>
     <?php Draw::Tabs(); ?>
-
-    <table class="table table-striped">
-        <tbody>
-        <?php foreach (Parser::getDatabase() as $record) {
-            $id = Parser::getIdFor($record['nazwa']);
-            $isActive = !empty($form_data[$id]['gram']);
-            ?>
-
-            <tr class="form_block <?php echo $record['typ']; ?>
-                    <?php echo $isActive ? '' : ' style="display: none;"'; ?>">
-
-                <td>
-                    <label for="<?php echo $id; ?>"
-                           class="col-lg-2 col-form-label"><?php echo $record['nazwa']; ?></label>
-                </td>
-                <td>
-                    <input class="form-control col-lg-2 <?php echo $isActive ? ' focus' : ''; ?>"
-                           type="number"
-                           style="width: 90px"
-                           name="<?php echo $id; ?>" min="1"
-                           value="<?php echo $form_data[$id]['gram']; ?>"/>
-                    <label for="<?php echo $id; ?>" class="control-label text-right">
-                </td>
-            </tr>
-        <?php } ?>
-        </tbody>
-    </table>
+    <?php Draw::TabContent(); ?>
 </form>
---
+++ draw/TabContent.php
@@ -0,0 +1,31 @@
+<!--    <div class="tab-content">-->
+<!--        <div role="tabpanel" class="tab-pane fade in active" id="home">...</div>-->
+
+<!--    btn-primary-->
+
+<table class="table table-striped">
+    <tbody>
+    <?php foreach (Parser::getDatabase() as $record) {
+        $id = Parser::getIdFor($record['nazwa']);
+        $isActive = !empty($form_data[$id]['gram']);
+        ?>
+
+        <tr class="form_block <?php echo $record['typ']; ?>
+                    <?php echo $isActive ? '' : ' style="display: none;"'; ?>">
+
+            <td>
+                <label for="<?php echo $id; ?>"
+                       class="col-lg-2 col-form-label"><?php echo $record['nazwa']; ?></label>
+            </td>
+            <td>
+                <input class="form-control col-lg-2 <?php echo $isActive ? ' focus' : ''; ?>"
+                       type="number"
+                       style="width: 90px"
+                       name="<?php echo $id; ?>" min="1"
+                       value="<?php echo $form_data[$id]['gram']; ?>"/>
+                <label for="<?php echo $id; ?>" class="control-label text-right">
+            </td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
