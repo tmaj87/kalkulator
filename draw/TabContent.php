425802bbcda7229b123b873d9f2f0f13a0c0483b
Tomasz Maj <tmaj.pl@gmail.com>
2016-10-25 22:18:12 +0200

    yess

--- draw/TabContent.php
+++ draw/TabContent.php
@@ -1,13 +1,13 @@
 <?php $types = array('olej', 'maslo', 'wosk', 'kwas'); ?>
-<div id="form_content" class="tab-content form_column">
+<div class="tab-content form_content">
     <?php foreach ($types as $type) { ?>
         <div role="tabpanel" class="tab-pane fade" id="<?php echo $type; ?>">
-            <table class="table table-striped">
+            <table class="table">
                 <tbody>
                 <?php foreach (Parser::getTabDataFor($type) as $id => $name) { ?>
                     <tr>
-                        <td>
-                            <input class="form-control" type="number" name="<?php echo $id; ?>" min="1">
+                        <td style="width: 20%; text-align: right">
+                            <input class="form-control" style="width: 110px" type="number" name="<?php echo $id; ?>" min="1">
                         </td>
                         <td class="text-left">
                             <label for="<?php echo $id; ?>" class="col-form-label"><?php echo $name; ?></label>
--- draw/Tabs.php
+++ draw/Tabs.php
@@ -1,6 +1,8 @@
-<ul id="form_tabs" class="nav nav-tabs form_column" role="tablist">
+<ul id="form_tabs" class="nav nav-tabs" role="tablist">
+    <li><span class="navbar-brand" style="margin-left: 40px">Waga (gramy)</span></li>
     <li role="presentation"><a href="#olej" aria-controls="settings" role="tab" data-toggle="tab">Oleje</a></li>
     <li role="presentation"><a href="#maslo" aria-controls="settings" role="tab" data-toggle="tab">Masła</a></li>
     <li role="presentation"><a href="#wosk" aria-controls="settings" role="tab" data-toggle="tab">Woski</a></li>
     <li role="presentation"><a href="#kwas" aria-controls="settings" role="tab" data-toggle="tab">Kwasy</a></li>
+    <li><input style="margin-left: 100px" class="navbar-brand btn" type="submit" value="Przelicz"></li>
 </ul>
